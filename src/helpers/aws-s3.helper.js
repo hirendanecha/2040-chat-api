7fbb9d38aa08180b5b790570f33f5c7d644df9d4
Swapnil Opash <sra.opash@gmail.com>
2024-03-01 15:42:29 +0530

    change bucket

--- src/helpers/aws-s3.helper.js
+++ src/helpers/aws-s3.helper.js
@@ -13,7 +13,7 @@ exports.uploadFileToWasabi = async (file, key) => {
       fs.readFile(file.path, function (err, buffer) {
         if (err) throw err; // Something went wrong!
         const params = {
-          Bucket: "freedom-social",
+          Bucket: "2040-chat",
           Key: key,
           Body: buffer,
         };
