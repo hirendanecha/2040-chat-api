b6efb2952e929644038249b6a8d5aff29db10ea4
htk-opash <htk.opash@gmail.com>
2024-03-15 13:00:35 +0530

    fixed decode user issue

--- src/common/common.js
+++ src/common/common.js
@@ -1,8 +1,10 @@
 const jwt = require("jsonwebtoken");
 const env = require("../environments/environment");
 
-exports.generateJwtToken = (payload, expiresIn) => {
+exports.generateJwtToken = (req, expiresIn) => {
   try {
+    const payload = {};
+    payload['user'] = req;
     return jwt.sign(payload, env.JWT_SECRET_KEY, { expiresIn });
   } catch (error) {
     return error;
