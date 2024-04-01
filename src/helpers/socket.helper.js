4593f17275df87af81f04d1d95aa8551e69c10c5
Swapnil Opash <sra.opash@gmail.com>
2024-04-01 16:28:15 +0530

    fixed

--- src/helpers/socket.helper.js
+++ src/helpers/socket.helper.js
@@ -534,6 +534,12 @@ socket.config = (server) => {
       try {
         if (params) {
           const data = await chatService.createGroups(params);
+          if (params.profileIds.length > 0) {
+            for (const id of params.profileIds) {
+              socket.join(`${id}`);
+            }
+          }
+          socket.join(`${data.groupId}`);
           console.log("group", data.notifications);
           if (data?.notifications) {
             for (const key in data?.notifications) {
